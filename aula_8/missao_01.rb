6dcc83e0c0a54fc4f4e0c1473a7420f121abcc9a
Gabriela Romualdo <gabimromualdo94@gmail.com>
2023-01-31 18:57:48 -0300

    correção missão

--- aula_8/missao_01.rb
+++ aula_8/missao_01.rb
@@ -2,3 +2,14 @@ names_lambda = -> (names){ names.each { |name| puts "O nome estava: #{name}, e a
 names = ["gabriela", "amanda", "alinne"]
 
 names_lambda.call(names)
+
+#correção aula:
+
+def capitalize_name(lambda_capitalize)
+    lambda_capitalize.call('leonardo')
+    lambda_capitalize.call('jose')
+end
+    
+lambda_capitalize = -> (name) { puts name.capitalize }
+    
+capitalize_name(lambda_capitalize)
