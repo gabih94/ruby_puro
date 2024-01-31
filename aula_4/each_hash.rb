9497a87479efcf2a59488c2dbf7da4fe55208f0d
gabih94 <gabimromualdo94@gmail.com>
2024-01-31 15:06:02 -0300

    refat

--- aula_4/each_hash.rb
+++ aula_4/each_hash.rb
@@ -1,4 +1,5 @@
-aulas = {'Aula 1 ' => 'liberada', 'Aula 2 ' => 'liberada', 'Aula 3 ' => 'liberada', 'Aula 4 ' => 'liberada', 'Aula 5 ' => 'em breve'}
+aulas = {'Aula 1 ': 'liberada', 'Aula 2 ': 'liberada', 'Aula 3 ': 'liberada', 'Aula 4 ': 'liberada', 'Aula 5 ': 'em breve'}
+# pode ser usado 'A' => valor ou 'A': valor
 
 aulas.each do |key, value|
     if value == 'em breve'
